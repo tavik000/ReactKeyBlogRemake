c4efa8243c5278269e2ea6cbaf6372b01f0b3b8c
key <tavik002@gmail.com>
2024-06-23 00:14:27 +0800

    database placeholder design

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -16,173 +16,54 @@ const customers = [
     email: 'delba@oliveira.com',
     image_url: '/customers/delba-de-oliveira.png',
   },
+];
+
+const post_tags = [
   {
-    id: '3958dc9e-742f-4377-85e9-fec4b6a6442a',
-    name: 'Lee Robinson',
-    email: 'lee@robinson.com',
-    image_url: '/customers/lee-robinson.png',
+    name: 'Other',
   },
   {
-    id: '3958dc9e-737f-4377-85e9-fec4b6a6442a',
-    name: 'Hector Simpson',
-    email: 'hector@simpson.com',
-    image_url: '/customers/hector-simpson.png',
-  },
-  {
-    id: '50ca3e18-62cd-11ee-8c99-0242ac120002',
-    name: 'Steven Tey',
-    email: 'steven@tey.com',
-    image_url: '/customers/steven-tey.png',
-  },
-  {
-    id: '3958dc9e-787f-4377-85e9-fec4b6a6442a',
-    name: 'Steph Dietz',
-    email: 'steph@dietz.com',
-    image_url: '/customers/steph-dietz.png',
-  },
-  {
-    id: '76d65c26-f784-44a2-ac19-586678f7c2f2',
-    name: 'Michael Novotny',
-    email: 'michael@novotny.com',
-    image_url: '/customers/michael-novotny.png',
-  },
-  {
-    id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
-    name: 'Evil Rabbit',
-    email: 'evil@rabbit.com',
-    image_url: '/customers/evil-rabbit.png',
-  },
-  {
-    id: '126eed9c-c90c-4ef6-a4a8-fcf7408d3c66',
-    name: 'Emil Kowalski',
-    email: 'emil@kowalski.com',
-    image_url: '/customers/emil-kowalski.png',
-  },
-  {
-    id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C144B9',
-    name: 'Amy Burns',
-    email: 'amy@burns.com',
-    image_url: '/customers/amy-burns.png',
-  },
-  {
-    id: '13D07535-C59E-4157-A011-F8D2EF4E0CBB',
-    name: 'Balazs Orban',
-    email: 'balazs@orban.com',
-    image_url: '/customers/balazs-orban.png',
+    name: 'UE',
   },
 ];
 
-const invoices = [
+const posts_en = [
   {
-    customer_id: customers[0].id,
-    amount: 15795,
-    status: 'pending',
-    date: '2022-12-06',
-  },
-  {
-    customer_id: customers[1].id,
-    amount: 20348,
-    status: 'pending',
-    date: '2022-11-14',
-  },
-  {
-    customer_id: customers[4].id,
-    amount: 3040,
-    status: 'paid',
-    date: '2022-10-29',
-  },
-  {
-    customer_id: customers[3].id,
-    amount: 44800,
-    status: 'paid',
-    date: '2023-09-10',
-  },
-  {
-    customer_id: customers[5].id,
-    amount: 34577,
-    status: 'pending',
-    date: '2023-08-05',
-  },
-  {
-    customer_id: customers[7].id,
-    amount: 54246,
-    status: 'pending',
-    date: '2023-07-16',
-  },
-  {
-    customer_id: customers[6].id,
-    amount: 666,
-    status: 'pending',
-    date: '2023-06-27',
-  },
-  {
-    customer_id: customers[3].id,
-    amount: 32545,
-    status: 'paid',
-    date: '2023-06-09',
-  },
-  {
-    customer_id: customers[4].id,
-    amount: 1250,
-    status: 'paid',
-    date: '2023-06-17',
-  },
-  {
-    customer_id: customers[5].id,
-    amount: 8546,
-    status: 'paid',
-    date: '2023-06-07',
-  },
-  {
-    customer_id: customers[1].id,
-    amount: 500,
-    status: 'paid',
-    date: '2023-08-19',
-  },
-  {
-    customer_id: customers[5].id,
-    amount: 8945,
-    status: 'paid',
-    date: '2023-06-03',
-  },
-  {
-    customer_id: customers[2].id,
-    amount: 8945,
-    status: 'paid',
-    date: '2023-06-18',
-  },
-  {
-    customer_id: customers[0].id,
-    amount: 8945,
-    status: 'paid',
-    date: '2023-10-04',
-  },
-  {
-    customer_id: customers[2].id,
-    amount: 1000,
-    status: 'paid',
-    date: '2022-06-05',
+    id: '3958dc9e-712f-4377-85e9-fec4b6a6442b',
+    post_title: 'Example Post',
+    thumbnail_img: '/customers/delba-de-oliveira.png',
+    post_tag: ['UE', 'Other'],
+    post_content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    author: 'Key',
+    comments: [
+      {
+        'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos possimus porro earum dolor sint fuga laborum velit laudantium distinctio quos sunt veritatis unde inventore, autem ad tenetur voluptatibus mollitia vel!',
+        'author': 'trollguy87',
+      },
+    ],
+    create_date: '2022-12-06',
   },
 ];
 
-const revenue = [
-  { month: 'Jan', revenue: 2000 },
-  { month: 'Feb', revenue: 1800 },
-  { month: 'Mar', revenue: 2200 },
-  { month: 'Apr', revenue: 2500 },
-  { month: 'May', revenue: 2300 },
-  { month: 'Jun', revenue: 3200 },
-  { month: 'Jul', revenue: 3500 },
-  { month: 'Aug', revenue: 3700 },
-  { month: 'Sep', revenue: 2500 },
-  { month: 'Oct', revenue: 2800 },
-  { month: 'Nov', revenue: 3000 },
-  { month: 'Dec', revenue: 4800 },
+const posts_ja = [
+  {
+    id: '3958dc9e-712f-4377-85e9-fec4b6a6442b',
+    post_title: '例の記事',
+    thumbnail_img: '/customers/delba-de-oliveira.png',
+    post_tag: ['UE', 'Other'],
+    post_content: '記事例文ですけど、芸フ都当切模不ネキウ製数あふそ由特べと名行めこも材誓コソ歳古ヱヤ奨読ラ援覧ヤヲク下文中ッしじ集和5割ぼようみ浜発文ルこせリ要順ーげつで。年エニ見7山ー中権ヤヒキ幌事防キル能場ぐふイ用臨キ社被ぱ奪三カケヱク込法ぶほ望阪ニト氏問クカラ中感投と。特に芸展び源速レコマワ調溶秀ち多兵くぞ付選ふちせご樫2間ぐおスし期住53横し応長つ術卒筋げまぜほ。8鳴あ予井くぱっう語説のすば予伴マカハフ遺飯どう渡記おら氷鳥げか総毎紀セ越問後迫ば特8禁ヌチヲル座電ヲメ情仁ふーげ。表エキヤ質一ク市亡ヤサチ票乳32民ぶルくへ融収イサ分室ラし楽貴ン勢地界ソノチミ監地っ供旅りくこで葬氷ゆほてゃ利化ロ北明列み覧化ソルモツ見並レヒセ害行披暑棚くどりえ。 暴る起芸お防位メマ質哀ろぼまを携死ワ効住べそぱ安購ご育覚ド相小ソロト欧放ニ聞課オクミノ航案スエセチ給際入色種さリや。97金審ヤサヘ柔2悪くぱひ放求好ユオ消査ヘクネ極傷ヤ闘旨能そみ山屋早崎ド。71竹テロイ無縮禁げすど売対ネマ能行こ七色年ぶ健事クリ館情おべ状7舞布益陣市ちも。 協げひざ域指クいず真経せにば調線ルカ況者ヲル風式ヒカ信揚布たがレ海7月メソニフ質念更ち点9人やねよ殊載ロヱハエ当関芸ふ豪児きのわ属王太ぱの片嘉塁塾ッなわ。第ふばやご始時ツアヨ田民のだも展体えむく出定べゆ栄場ツ遂違ヲ世本名ニヌキケ壊必テ前光チキ佐三ちフぽ亀報ネニ能投32園ぞぼおル役射窃ぱッび。 年だ士抵ゅドラ週済ニイフヘ樹民ふざレ組更シヲ保営ッ討鉄ヘ掲島森さにた展際ろばぽク金不ナ響述クナヒ注情こ小映傷込体左ゃをしま。善くあ県変を月派路高じなでッ安米ル入町ごたぎな予森ルセ入戦しな文罪ミホニ竹独ヘマオ真重海決取いだ。読かまン解道38勢土工幅7第モチ工都エチ済石ラ仕連じ結4警をじこあ女9日マ図索りぞ面事ざトにぼ著主レイヲネ壇信立がーどト。 絞ネヤラフ宮聞ツムクハ気今テニ平調ハケリソ統新4必はぐクら常展けぽせ久幕のごろな理初ちもむほ質鉄コ上職架津ひる要禁ぞゃルさ編明ネオモ職今ざク木天浩簡様志おじ。2選ルツレク拙記ヘオ雪転味ぽぎざ喜容いへ横写ヤミヘ界果や社東なち憶説東てえフ資護枠ど。際ねむ衛真さおはま気両ハシラ北署モ離授タ月似とレ浜航わおず館愛亡39動請ヘエム古禁ヨ神伊どッけ。 量ニネ代応ノ佑頭らのゅ弘活中ワモハ堀治っ策皇ン厳2思ぴさッ逆護め詳腰どず号造エイウカ宮4免在稼エコヤ携物をもあ束央ぶせべよ残治フリユサ新本ちよけ本玲講芝伯ばはよ。修カニ成佐ワハミチ載少サユ比法あな周将ふやむ案料辺ヲト都96歳持脱71来もしー二金療しべで記生想トエヌフ同強ヤヱケ弔禁れ作着のへさゃ城傑レぜひぎ。 果キクセ代方ヲ吉差ぐかい反購レヒ降行6綸土つうばん上沢かんラる万県っ紙84能ふ住置ドてまク年謙ウレヒ医報育んと牡禁くらへこ所少ルラ会白象留れこ。旅すせもク半堀ヨ梨負ヨキ与時ヤワミ競点あだせわ我雄らで程新サ看夜り択室岸クのまも木野けらを年者ラ館長ナヘサヲ具五肉う東備ヘノ法今部モ政法視劣嘆せ。 務オソ締会9感波ハサヨ日続うげルが品啓さま第月追レ聞知ヒロ平意コヌ足50判せよぼ占聞ゆ際起1能コシ行成ざ送断社ぞ。拝ご球住除ウ日選ぼへでも僕倉ロエモツ阜用に後上ちぶっづ層7最ワヒセハ青8得びをレ読気ぽ聴注岡きろし。言サキセコ金夏べめ間害ケル目断経キ訳経止クぎす普効えに題京両シハス故倒つづ著謙ナ町涯ヱホ氏割けへ高城要ぴぞか国民むす表干悟栽桂ぼ。 扱シキヤワ読動は禁合す貴辺ば百人ぎべゅで真記ツサテケ氏事イ馬後オム見残要3電ひくだ交版トせク回8断イほひ土諭ウニトセ版著ヤネ辺企ゃぞく勝校フ載調敷俸をつら。態ワウモフ地更ゃレ団委お演実ナラ復励宮カロホ込首だ渇3姿ゅめち景出著禁7込ッあぐ索回触ふ載役総ら。開ヲ新国ヘ亡載ヌヱ図本アルテ買京べりッ氏目ド載図回タ水毎ぎぜ梨学9患くた吉円ろ。',
+    author: 'Key',
+    comments: [
+      {
+        'body':'記事例文ですけど、芸フ都当切模不ネキウ製数あふそ由特べと名行めこも材誓コソ歳古ヱヤ奨読ラ援',
+        'author': 'trollguy87',
+      },
+    ],
+    create_date: '2022-12-06',
+  },
 ];
 
 module.exports = {
   users,
   customers,
-  invoices,
-  revenue,
 };
