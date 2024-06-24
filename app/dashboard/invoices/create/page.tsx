e2054eb4b24e1efb3aa33d300b132f1b25c8e924
key <tavik002@gmail.com>
2024-06-24 16:06:04 +0800

    backup

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,4 +1,4 @@
-import { fetchCustomers } from '@/app/lib/data';
+import { fetchCustomers } from '@/app/lib/data_backup';
 import Form from '@/app/ui/invoices/create-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { Metadata } from 'next';
