519492d88da34cff486b40b2a37a9b9265112af3
key <tavik002@gmail.com>
2024-06-24 16:06:31 +0800

    fix connection terminated unexpectly; use sql instead of client.query

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -13,48 +13,110 @@ import { formatCurrency } from './utils';
 import { unstable_noStore as noStore } from 'next/cache';
 import format from 'pg-format';
 
+// export async function connectDatabase() {
+//   let retries = 5;
+//   while (retries) {
+//     try {
+//       const client = await db.connect();
+//       // Your database query code here...
+//       return client;
+//     } catch (error) {
+//       console.error('Database Error:', error);
+//       retries -= 1;
+//       console.log(`Retries left: ${retries}`);
+//       if (!retries) throw new Error('Failed to connect to the database.');
+//       // Wait for 1 second before retrying
+//       await new Promise((res) => setTimeout(res, 1000));
+//     }
+//   }
+// }
+
 const ITEMS_PER_PAGE: number = 9;
 export async function fetchFilteredPosts(
   query: string,
   currentPage: number,
   locale: string,
 ) {
+  console.log('fetchFilteredPosts');
   noStore();
-  const client = await db.connect();
+
   const offset: number = (currentPage - 1) * ITEMS_PER_PAGE;
 
-    const selectPostsQuery = format(
-      `
-      SELECT
-        id,
-        title,
-        thumbnail_img,
-        tags,
-        create_date
-      FROM posts_%I
-      WHERE
-        title ILIKE %L OR
-        content ILIKE %L
-      ORDER BY create_date DESC
-      LIMIT %L OFFSET %L
-      `,
-      locale,
-      `%${query}%`,
-      `%${query}%`,
-      ITEMS_PER_PAGE,
-      offset
-    );
-
   try {
-    const posts = await client.query<PostCard>(selectPostsQuery);
-
+    var posts;
+    switch (locale) {
+      case 'en':
+        posts = await sql<PostCard>`
+          SELECT
+            id,
+            title,
+            thumbnail_img,
+            tags,
+            create_date
+          FROM posts_en
+          WHERE
+            title ILIKE ${`%${query}%`} OR
+            content ILIKE ${`%${query}%`}
+          ORDER BY create_date DESC
+          LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset} 
+        `;
+        break;
+      case 'ja':
+        posts = await sql<PostCard>`
+          SELECT
+            id,
+            title,
+            thumbnail_img,
+            tags,
+            create_date
+          FROM posts_jp
+          WHERE
+            title ILIKE ${`%${query}%`} OR
+            content ILIKE ${`%${query}%`}
+          ORDER BY create_date DESC
+          LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset} 
+        `;
+        break;
+      case 'kr':
+        posts = await sql<PostCard>`
+          SELECT
+            id,
+            title,
+            thumbnail_img,
+            tags,
+            create_date
+          FROM posts_kr
+          WHERE
+            title ILIKE ${`%${query}%`} OR
+            content ILIKE ${`%${query}%`}
+          ORDER BY create_date DESC
+          LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset} 
+        `;
+      case 'zh':
+        posts = await sql<PostCard>`
+          SELECT
+            id,
+            title,
+            thumbnail_img,
+            tags,
+            create_date
+          FROM posts_zh
+          WHERE
+            title ILIKE ${`%${query}%`} OR
+            content ILIKE ${`%${query}%`}
+          ORDER BY create_date DESC
+          LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset} 
+        `;
+      default:
+        throw new Error('Unsupported locale.');
+    }
     return posts.rows;
   } catch (error) {
     console.error('Database Error:', error);
+    console.log('offset: ' + offset + ', query: ' + query);
     throw new Error('Failed to fetch filtered posts.');
   }
 }
-
 
 export async function getUser(email: string) {
   try {
