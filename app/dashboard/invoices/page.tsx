402b33740900488f9df8c08144d488ad736e5a21
Key <tavik002@gmail.com>
2024-07-07 10:15:52 +0900

    search ref

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -31,11 +31,11 @@ export default async function Page({
       <div className="flex w-full items-center justify-between">
         <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
       </div>
-      {/* <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
-      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+      {/*<Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
       </Suspense> */}
       <div className="mt-5 flex w-full justify-center">
