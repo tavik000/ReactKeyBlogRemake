8eb6e7ba3669dd260f36a1b1def4b4d89b0b35b7
Key <tavik002@gmail.com>
2024-07-09 22:02:20 +0900

    add async and mian tag

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,8 +1,9 @@
-export default function Page({ params }: { params: { id: string } }) {
-
+export default async function Page({ params }: { params: { id: string } }) {
   return (
-    <div className="flex min-h-screen flex-col">
-      <h1>Page</h1>
-    </div>
+    <main>
+      <div className="flex min-h-screen flex-col">
+        <h1>Page</h1>
+      </div>
+    </main>
   );
 }
