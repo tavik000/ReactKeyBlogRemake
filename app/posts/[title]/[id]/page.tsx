c3720739479bd2dc9132edb1956e40d3abcfcb05
Key <tavik002@gmail.com>
2024-07-13 12:52:14 +0900

    post

--- app/posts/[title]/[id]/page.tsx
+++ app/posts/[title]/[id]/page.tsx
@@ -6,7 +6,14 @@ export default async function Page({
   return (
     <main>
       <div className="flex min-h-screen flex-col">
-        <h1>Page</h1>
+        <div className="relative mt-6 flex justify-center md:flex-row">
+          <div className="flex w-10/12 max-w-1140px basis-2/3 rounded-xl bg-white px-12 pb-8 pt-8 shadow-0550">
+            <div className="flex flex-col">
+              <p className="flex pb-4 text-xl tracking-widest">Post</p>
+              <div className="flex flex-col"></div>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
