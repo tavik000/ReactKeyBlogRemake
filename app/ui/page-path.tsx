4ede2c6479bb7f0fc60e46f94502c8655b836ad1
Key <tavik002@gmail.com>
2024-07-13 22:23:47 +0900

    page path accept post title

--- app/ui/page-path.tsx
+++ app/ui/page-path.tsx
@@ -1,33 +1,68 @@
+'use client';
+
 import { homepageURL } from '@/app/lib/constants';
 import Link from 'next/link';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import path from 'path';
 
 export function PagePath() {
-    return (
+  const pathname = usePathname();
+  const pathArray = pathname.split('/').filter(Boolean);
+  let postTitle: string = '';
 
-      <div className="page-path flex justify-center relative z-10 mb-0 py-3">
-        <ul className="w-10/12 basis-2/3 max-w-1140px mx-auto list-none rounded-md bg-white">
-          <PagePathItem url={homepageURL} shouldShowArrow={true}>Key Homepage</PagePathItem>
-          <PagePathItem url='/' shouldShowArrow={false}>Blog</PagePathItem>
-        </ul>
+  if (pathArray.length >= 3) {
+    pathArray[1] = pathArray[1].replace(/-/g, ' ');
+    postTitle = pathArray[1];
+  }
+
+  return (
+    <div className="page-path relative z-10 mb-0 flex justify-center py-3">
+      <div className="flex flex-row py-1 px-2.5 mx-auto w-10/12 max-w-1140px basis-2/3 list-none rounded-md bg-white">
+        <PagePathItem url={homepageURL} shouldShowArrow={true} isPost={false}>
+          Key Homepage
+        </PagePathItem>
+        {pathArray.length >= 3 ? (
+          <div className="flex flex-row">
+            <PagePathItem url="/" shouldShowArrow={true} isPost={false}>
+              Blog
+            </PagePathItem>
+            <PagePathItem url="" shouldShowArrow={false} isPost={true}>
+              {postTitle}
+            </PagePathItem>
+          </div>
+        ) : (
+          <PagePathItem url="/" shouldShowArrow={false} isPost={false}>
+            Blog
+          </PagePathItem>
+        )}
       </div>
-    );
+    </div>
+  );
 }
 
 function PagePathItem({
   children,
   shouldShowArrow,
   url,
+  isPost,
 }: {
   children: string;
   shouldShowArrow?: boolean;
   url: string;
+  isPost: boolean;
 }) {
-  return (
-    <li className="mx-2.5 inline text-sm">
+  return !isPost ? (
+    <li className="inline text-sm">
       <Link href={url} className="text-orange-400 hover:underline">
         <span>{children}</span>
       </Link>
-      {shouldShowArrow && <span className="page-path-arrow ml-3.5">&gt;</span>}
+      {shouldShowArrow && <span className="page-path-arrow mx-2">&gt;</span>}
+    </li>
+  ) : (
+    <li className="inline text-sm">
+      <div className="text-black">
+        <span>{children}</span>
+      </div>
     </li>
   );
 }
