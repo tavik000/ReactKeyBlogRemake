da3a4ceb495a8cbc3a322d14941c17026c95fcf2
Key <tavik002@gmail.com>
2024-07-14 15:40:30 +0900

    Revert "install markdown to jsx"
    This reverts commit cd61624d015939fe3007af92cdcadca396e4b261.

--- app/posts/[title]/[id]/page.tsx
+++ app/posts/[title]/[id]/page.tsx
@@ -41,7 +41,6 @@ export default async function Page({ params }: { params: { id: string } }) {
               />
             </div>
           </div>
-          
         </div>
       </div>
     </main>
