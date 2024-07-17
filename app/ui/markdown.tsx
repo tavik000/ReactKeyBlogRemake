20d757add0d25727fa24258fd417e072c8a4f5cd
Key <tavik002@gmail.com>
2024-07-18 00:45:57 +0900

    markdown support code line highlight

--- app/ui/markdown.tsx
+++ app/ui/markdown.tsx
@@ -12,6 +12,11 @@ type MarkdownRendererProps = {
   children: string;
 };
 
+const customStyle = {
+  fontSize: '1rem',
+  padding: '20px',
+};
+
 export function MarkdownRenderer({
   children: markdown,
 }: MarkdownRendererProps) {
@@ -25,12 +30,27 @@ export function MarkdownRenderer({
         code({ node, inline, className, children, ...props }: any) {
           const match = /language-(\w+)/.exec(className || '');
           const code = String(children).replace(/\n$/, '');
-          const filename = 'app/ui/markdown.tsx';
+          const filename = match?.input.split(':')[1];
+          const linesToHighlightInput = match?.input.split(':')[2];
+          const linesToHighlight = linesToHighlightInput
+            ? linesToHighlightInput.split(',').reduce((acc, line) => {
+                if (line.includes('-')) {
+                  const [start, end] = line.split('-').map(Number);
+                  for (let i = start; i <= end; i++) {
+                    acc.push(i);
+                  }
+                } else {
+                  acc.push(Number(line));
+                }
+                return acc;
+              }, [] as number[])
+            : [];
 
+          console.log(linesToHighlight);
           return !inline && match ? (
             <Fragment>
               {filename && (
-                <div className="-mb-6 px-3 pb-4 pt-1 table max-w-full rounded-lg bg-slate-700 text-white text-sm">
+                <div className="-mb-6 table max-w-full rounded-lg bg-slate-700 px-3 pb-4 pt-1 text-sm text-white">
                   <span>{escapeHtml(filename)}</span>
                 </div>
               )}
@@ -42,8 +62,22 @@ export function MarkdownRenderer({
               >
                 <SyntaxHighlighter
                   style={oneDark}
+                  customStyle={customStyle}
+                  showLineNumbers
                   PreTag="div"
                   language={match[1]}
+                  wrapLines
+                  lineProps={(lineNumber) => {
+                    const style: any = {
+                      display: 'block',
+                      width: 'fit-content',
+                    };
+                    if (linesToHighlight.includes(lineNumber)) {
+                      style.backgroundColor = '#334155';
+                      style.borderColor = '#38bdf8';
+                    }
+                    return { style };
+                  }}
                   {...props}
                 >
                   {code}
