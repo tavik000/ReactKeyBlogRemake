219af1b04ce3f46502881a4787a171a6ad34ceb9
Key <tavik002@gmail.com>
2024-07-22 23:08:28 +0900

    downgrade to next 14 and react and dom

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,14 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
-
+import { match } from '@formatjs/intl-localematcher';
+import Negotiator from 'negotiator';
+ 
+let headers = { 'accept-language': 'en-US,en;q=0.5' }
+let languages = new Negotiator({ headers }).languages()
+let locales = ['en-US', 'nl-NL', 'nl']
+let defaultLocale = 'en-US'
+ 
+match(languages, locales, defaultLocale) // -> 'en-US'
 export default NextAuth(authConfig).auth;
 
 export const config = {
