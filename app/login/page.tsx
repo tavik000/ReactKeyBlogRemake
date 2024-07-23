ecc84ad28d08d25ad0dd583db1e8ba79c8c3fe35
Key <tavik002@gmail.com>
2024-07-24 01:54:10 +0900

    remove main

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,7 @@ import LoginForm from '@/app/ui/login-form';
 
 export default function LoginPage() {
   return (
-    <main className="flex items-center justify-center md:h-screen">
+    // <main className="flex items-center justify-center md:h-screen">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
         <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
           <div className="w-32 text-white md:w-36">
@@ -12,6 +12,6 @@ export default function LoginPage() {
         </div>
         <LoginForm />
       </div>
-    </main>
+    // </main>
   );
 }
