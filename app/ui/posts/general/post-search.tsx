cec6cfba56dca2e4f43da5d148e0d9decbfa4c44
Key <tavik002@gmail.com>
2024-07-31 23:48:55 +0900

    post search handle enter key

--- app/ui/posts/general/post-search.tsx
+++ app/ui/posts/general/post-search.tsx
@@ -4,6 +4,7 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 import { GetLangFromLocale } from '@/app/lib/constants';
+import React, { useEffect } from 'react';
 
 export default function PostSearch({
   locale,
@@ -33,13 +34,15 @@ export default function PostSearch({
     }
     replace(`/${lang}/?${params.toString()}`);
   }, 300);
-  
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      handleSearch(e.currentTarget.value);
+    }
+  };
 
   return (
     <div className="relative z-auto my-2 ml-6 flex h-10 max-h-16 max-w-sm flex-1 flex-shrink-0 rounded-full py-0.5">
-      <label htmlFor="search" className="sr-only">
-        Search
-      </label>
+      <label className="sr-only">Search</label>
       <input
         id="post-search"
         className="peer block w-full rounded-md border border-gray-200 bg-gray-100 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
@@ -47,6 +50,7 @@ export default function PostSearch({
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
+        onKeyDown={handleKeyDown} 
         defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
