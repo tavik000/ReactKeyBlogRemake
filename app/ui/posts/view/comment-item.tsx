10d14035e5759b5be2453b0f3e31f947743714b8
Key <tavik002@gmail.com>
2024-09-13 00:10:51 +0900

    comment item wip

--- app/ui/posts/view/comment-item.tsx
+++ app/ui/posts/view/comment-item.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { PostComment } from "@/app/lib/definitions";
+import { HeartIcon } from '@heroicons/react/24/outline';
 import { User } from "@nextui-org/react";
+import { MarkdownRenderer } from '../../markdown';
+import { InteractIcon } from '@/app/ui/posts/view/interaction';
 
 export default function CommentItem({
   // comment,
@@ -14,8 +17,8 @@ export default function CommentItem({
 
 
   return (
-    <div className="mt-6 flex justify-between border-b-2 border-t-2 border-gray-100">
-      <div className="flex content-center w-full">
+    <div className="mt-6 flex-col justify-between border-b-2 border-t-2 border-gray-100">
+      <div className="flex mt-4 content-center w-full">
         <User
           name="User"
           avatarProps={{
@@ -32,11 +35,25 @@ export default function CommentItem({
             .join(' ')
             .replace(/(?<=\d) /, ', ')}
         </p>
+
       </div>
 
       {/* Markdown */}
-      <div className="flex content-center">
+      <div className="flex mt-4 content-center">
         <div className="mr-0 inline-block flex-shrink-0">
+          <MarkdownRenderer> A Markdown Post</MarkdownRenderer>
+        </div>
+      </div>
+
+      <div className="flex mt-4 mb-4 content-center">
+        <div className="mr-0 inline-block flex-shrink-0">
+          <InteractIcon count={1} shouldShowCount={true}>
+            <HeartIcon
+              className="flex h-6 w-6 align-middle"
+              color="#757575"
+              title="Like"
+            />
+          </InteractIcon>
         </div>
       </div>
 
