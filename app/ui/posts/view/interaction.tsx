17addc9bfab79d442d69555927b4d96811af8a0e
Key <tavik002@gmail.com>
2024-09-12 23:13:06 +0900

    move interaction to new file

---
+++ app/ui/posts/view/interaction.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+export const InteractIcon = ({
+  count,
+  shouldShowCount,
+  children,
+}: {
+  count: number;
+  shouldShowCount: boolean;
+  children: React.ReactNode;
+}) => {
+  return (
+    <div className="inline-block flex-shrink-0 w-16 py-2 px-0.5 break-words">
+      <div className="flex flex-row content-center">
+        <Link href="/" className="flex">
+          <div className="relative mr-1">
+            <span>
+              <div className="inline-block align-middle">{children}</div>
+            </span>
+          </div>
+          {shouldShowCount && (
+            <div className="block break-words">
+              <div className="inline-block">
+                <p className="flex">{count}</p>
+              </div>
+            </div>
+          )}
+        </Link>
+      </div>
+    </div>
+  );
+};
--- app/ui/posts/view/post-interaction.tsx
+++ app/ui/posts/view/post-interaction.tsx
@@ -1,6 +1,7 @@
 import { HeartIcon, ChatBubbleLeftIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { ShareButton } from '@/app/ui/button';
+import { InteractIcon } from '@/app/ui/posts/view/interaction';
 
 export default function PostInteraction({
   likeCount,
@@ -35,34 +36,3 @@ export default function PostInteraction({
     </div>
   );
 }
-
-const InteractIcon = ({
-  count,
-  shouldShowCount,
-  children,
-}: {
-  count: number;
-  shouldShowCount: boolean;
-  children: React.ReactNode;
-}) => {
-  return (
-    <div className="inline-block flex-shrink-0 w-16 py-2 px-0.5 break-words">
-      <div className="flex flex-row content-center">
-        <Link href="/" className="flex">
-          <div className="relative mr-1">
-            <span>
-              <div className="inline-block align-middle">{children}</div>
-            </span>
-          </div>
-          {shouldShowCount && (
-            <div className="block break-words">
-              <div className="inline-block">
-                <p className="flex">{count}</p>
-              </div>
-            </div>
-          )}
-        </Link>
-      </div>
-    </div>
-  );
-};
