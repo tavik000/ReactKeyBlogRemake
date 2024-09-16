ccdebbf4cef53c99909bb9272bbc3e87d733cc66
Key <tavik002@gmail.com>
2024-09-16 18:02:42 +0900

    remove unused

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -2,8 +2,6 @@ import PostOverview from '@/app/ui/posts/overview/post-overview';
 import { GetLocaleFromLang } from '@/app/lib/constants';
 import { Suspense } from 'react';
 import PostOverviewSkeleton from '@/app/ui/posts/overview/post-overview-skeleton';
-import { getDictionary } from '@/app/components/localization/dictionaries';
-import { DictStructure } from '@/app/components/localization/dict-store';
 
 export default function Page({
   searchParams,
@@ -21,11 +19,9 @@ export default function Page({
 
   return (
     <main className="light text-foreground bg-background flex min-h-screen flex-col">
-      {/* <LocaleProvider inLocale={locale} inLang={lang} inDict={dict}> */}
         <Suspense fallback={<PostOverviewSkeleton />}>
           <PostOverview searchParams={searchParams} locale={locale} />
         </Suspense>
-      {/* </LocaleProvider> */}
     </main>
   );
 }
