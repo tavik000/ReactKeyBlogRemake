619e526176068c10e69311cf19fc79d51b8afd0b
Key <tavik002@gmail.com>
2024-09-16 18:44:46 +0900

    login comment ui wip

--- app/ui/posts/view/login-comment-form.tsx
+++ app/ui/posts/view/login-comment-form.tsx
@@ -2,18 +2,35 @@
 import { Button } from '@/app/ui/button';
 import { useLoginOpenFromPostContext } from '@/app/components/context/login-open-from-post-provider';
 import { useSessionContext } from '@/app/components/context/session-provider';
-
-
+import { Avatar } from '@nextui-org/react';
+import { useLocaleContext } from '@/app/components/context/locale-provider';
 
 export default function LoginCommentForm() {
 
+    const { dict } = useLocaleContext();
     const { session } = useSessionContext();
     const { setIsLoginOpenFromPost } = useLoginOpenFromPostContext();
 
     return (
         <div>
-            {session && session?.user ? (
-                <></>
+            {session && session?.user && session.user?.image ? (
+                <>
+                    {/* localization */}
+                    <div className="flex-col mt-4">
+                        <div className="flex">
+                            <Avatar
+                                src={session.user.image}
+                                size='sm'
+                            />
+                            <p className="ml-2 text-center align-middle justify-center">comment something...</p>
+                        </div>
+                        <div>
+
+                        </div>
+
+
+                    </div>
+                </>
             ) : (
                 <>
                     <div className="flex-col rounded-lg mt-4 p-6 text-center bg-gray-200">
