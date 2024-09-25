5d441566174fd931a8919d47c4ce62e3efe87196
Key <tavik002@gmail.com>
2024-09-25 10:03:15 +0900

    notificaiton dropdown ui by shadcn-ui

--- app/ui/posts/general/notification-button.tsx
+++ app/ui/posts/general/notification-button.tsx
@@ -7,20 +7,25 @@ import { useSessionContext } from "@/app/components/context/session-provider";
 import { useLocaleContext } from '@/app/components/context/locale-provider';
 import { BellIcon } from "@heroicons/react/24/solid";
 
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button"
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuPortal,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
-  DropdownMenuTrigger,
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuPortal,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+    DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
 export function NotificationButton() {
@@ -64,41 +69,39 @@ export function NotificationButton() {
 
     return (
         <div className="mr-6 flex items-center gap-8 text-black ">
-            <DropdownMenu >
-                <DropdownMenuTrigger>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
                     <BellIcon className="h-6 w-6 text-gray-500 hover:text-orange-500 hover:cursor-pointer" />
                 </DropdownMenuTrigger>
                 {/* TODO localization */}
-                <DropdownMenuContent className="p-3 w-80" >
+                <DropdownMenuContent align="start" className="p-3 w-80" >
                     {/* <DropdownSection title={dict.notification.title} /> */}
 
-                        <DropdownMenuLabel>Notification</DropdownMenuLabel>
-                        <DropdownMenuSeparator />
+                    <DropdownMenuLabel>Notification</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
                     <DropdownMenuGroup title="Notification" className="">
                         {notifications.map((notification) => (
                             <DropdownMenuItem
                                 key={notification.id}
                             >
-                                    <User
-                                        name={notification.source_user_name}
-                                        avatarProps={{
-                                            size: 'sm',
-                                            src: notification.source_user_img,
-                                        }}
-                                    />
-                                <div className="w-32">
+                                <Avatar className="w-8 h-8">
+                                    <AvatarImage src={notification.source_user_img} alt={notification.source_user_name} />
+                                    <AvatarFallback>{notification.source_user_name}</AvatarFallback>
+                                </Avatar>
+                                <div className="ml-4 w-80">
                                     <p>{notification.comment_content}</p>
                                 </div>
-                            </DropdownItem>
+                            </DropdownMenuItem>
                         ))}
-                    </DropdownSection>
-                    <DropdownSection title="Delete">
-                        <DropdownItem key="delete" className="text-red-500 hover:bg-gray-200" color="danger">
+                    </DropdownMenuGroup>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuGroup title="Delete">
+                        <DropdownMenuItem key="delete" className="text-red-500 hover:bg-gray-200" color="danger">
                             Delete All Notifications
-                        </DropdownItem>
-                    </DropdownSection>
-                </DropdownMenu>
-            </Dropdown>
-        </div>
+                        </DropdownMenuItem>
+                    </DropdownMenuGroup>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </div >
     );
 }
