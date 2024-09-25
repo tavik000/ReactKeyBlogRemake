8777a0965c1fada5273ee44d5d58868df0bc86fb
Key <tavik002@gmail.com>
2024-09-25 23:27:03 +0900

    notificaiton content, update ui

--- app/ui/posts/general/notification-button.tsx
+++ app/ui/posts/general/notification-button.tsx
@@ -2,11 +2,9 @@
 
 import { useNotificationContext } from "@/app/components/context/notification-provider";
 import { useSessionContext } from "@/app/components/context/session-provider";
-// import { Dropdown, DropdownMenu, DropdownTrigger, DropdownItem, DropdownSection, User } from "@nextui-org/react";
-
 import { useLocaleContext } from '@/app/components/context/locale-provider';
 import { BellIcon } from "@heroicons/react/24/solid";
-
+import { Notification } from "@/app/lib/definitions";
 import {
     Avatar,
     AvatarFallback,
@@ -27,8 +25,9 @@ import {
     DropdownMenuSubTrigger,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { keyName } from "@/app/lib/constants";
 
-export function NotificationButton( { isHidden }: { isHidden: boolean }) {
+export function NotificationButton({ isHidden }: { isHidden: boolean }) {
 
     const { dict } = useLocaleContext();
     const { notifications } = useNotificationContext();
@@ -38,6 +37,44 @@ export function NotificationButton( { isHidden }: { isHidden: boolean }) {
 
     if (!isLogin || isHidden) {
         return null;
+    }
+
+    function truncateText(text: string, maxLength: number) {
+        if (text.length > maxLength) {
+            return text.substring(0, maxLength) + '...';
+        }
+        return text;
+    }
+
+    const NotificationFormatContent = ({ notification }: { notification: Notification }) => {
+        // TODO localization
+        const truncatedPostTitle = truncateText(notification.post_title, 100);
+        let truncatedCommentContent = '';
+        if (notification.comment_content) {
+            truncatedCommentContent = truncateText(notification.comment_content, 100);
+        }
+        if (notification.type === 'comment') {
+            if (notification.target_user_name === keyName) {
+                return (
+                    <p>{notification.source_user_name} <strong>commented</strong> on your article <strong>{truncatedPostTitle}</strong>: &quot;{truncatedCommentContent}&quot;</p>
+                )
+            }
+            else {
+                return (
+                    <p>{notification.source_user_name} <strong>commented</strong> on the article you&apos;re subscribed to: <strong>{truncatedPostTitle}</strong></p>
+                )
+            }
+        } else if (notification.type === 'like') {
+            if (notification.comment_id == null) {
+                return (
+                    <p>{notification.source_user_name} <strong>liked</strong> your article <strong>{truncatedPostTitle}</strong></p>
+                )
+            } else {
+                return (
+                    <p>{notification.source_user_name} <strong>liked</strong> your comment: &quot;{truncatedCommentContent}&quot;</p>
+                )
+            }
+        }
     }
 
     if (!notifications || notifications.length === 0) {
@@ -85,20 +122,35 @@ export function NotificationButton( { isHidden }: { isHidden: boolean }) {
                             <DropdownMenuItem
                                 key={notification.id}
                             >
-                                <Avatar className="w-8 h-8">
-                                    <AvatarImage src={notification.source_user_img} alt={notification.source_user_name} />
-                                    <AvatarFallback>{notification.source_user_name}</AvatarFallback>
-                                </Avatar>
-                                <div className="ml-4 w-80">
-                                    <p>{notification.comment_content}</p>
+                                <div className="mt-2 flex flex-row hover:cursor-pointer">
+                                    <Avatar className="flex w-8 h-8">
+                                        <AvatarImage src={notification.source_user_img} alt={notification.source_user_name} />
+                                        <AvatarFallback>{notification.source_user_name}</AvatarFallback>
+                                    </Avatar>
+                                    <div className="flex flex-col ml-4">
+                                        <div className="flex">
+                                            <NotificationFormatContent notification={notification} />
+                                        </div>
+                                        <div className="flex mt-1">
+                                            <p className="text-gray-400 text-xs">{notification.create_date
+                                                .toDateString()
+                                                .split(' ')
+                                                .slice(1)
+                                                .join(' ')
+                                                .replace(/(?<=\d) /, ', ')}
+                                            </p>
+                                        </div>
+                                    </div>
                                 </div>
                             </DropdownMenuItem>
                         ))}
                     </DropdownMenuGroup>
                     <DropdownMenuSeparator />
                     <DropdownMenuGroup title="Delete">
-                        <DropdownMenuItem key="delete" className="text-red-500 hover:bg-gray-200" color="danger">
-                            Delete All Notifications
+                        <DropdownMenuItem key="delete" className="text-gray-600 hover:bg-gray-200" color="danger">
+                            <div className="flex items-center justify-center align-middle w-full hover:cursor-pointer">
+                                <p className="flex text-center font-bold">Clear All Notifications</p>
+                            </div>
                         </DropdownMenuItem>
                     </DropdownMenuGroup>
                 </DropdownMenuContent>
