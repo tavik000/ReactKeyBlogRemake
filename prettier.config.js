654ecc31eb17dd310d95795c4ce920e7dfd835d2
Key <tavik002@gmail.com>
2024-10-01 15:15:35 +0900

    tab width 2

--- prettier.config.js
+++ prettier.config.js
@@ -3,5 +3,4 @@ const styleguide = require('@vercel/style-guide/prettier');
 module.exports = {
   ...styleguide,
   plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
-  tabWidth: 4,
 };
