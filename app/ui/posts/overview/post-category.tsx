c15d83dca36a65608f7d9296f6d2db693cc7f798
Key <tavik002@gmail.com>
2024-10-02 10:11:04 +0900

    post item rwd min h

--- app/ui/posts/overview/post-category.tsx
+++ app/ui/posts/overview/post-category.tsx
@@ -4,56 +4,41 @@ import { fetchPostTags } from "@/app/lib/data";
 import { PostTagItem } from "@/app/ui/posts/general/post-tag";
 
 export default async function PostCategory({ locale }: { locale: string }) {
+  const dict = (await getDictionary(locale)) as DictStructure;
 
-    const dict = (await getDictionary(locale)) as DictStructure;
+  const postTags: string[] = await fetchPostTags();
+  const third = Math.ceil(postTags.length / 3);
+  const firstPart = postTags.slice(0, third - 1);
+  const secondPart = postTags.slice(third - 1, third * 2 - 1);
+  const thirdPart = postTags.slice(third * 2 - 1);
 
-    const postTags: string[] = await fetchPostTags();
-    const third = Math.ceil(postTags.length / 3);
-    const firstPart = postTags.slice(0, third - 1);
-    const secondPart = postTags.slice(third - 1, third * 2 - 1);
-    const thirdPart = postTags.slice(third * 2 - 1);
-
-    return (
-        <div className="category mt-6 flex md:flex-row">
-            <div className="flex w-10/12 max-w-1140px basis-2/3 rounded-xl bg-white px-12 pb-8 pt-8 shadow-0550 xs:min-w-[300px]">
-                <div className="flex flex-col">
-                    <p className="flex pb-4 text-xl tracking-widest">
-                        {dict.category}
-                    </p>
-                    <div className="flex flex-col">
-                        <div className="flex xs:flex-col md:flex-row">
-                            <ul className="w-80">
-                                <PostTagItem tag="All" isLabel={false} />
-                                {firstPart.map((tag) => (
-                                    <PostTagItem
-                                        key={tag}
-                                        tag={tag}
-                                        isLabel={false}
-                                    />
-                                ))}
-                            </ul>
-                            <ul className="w-80">
-                                {secondPart.map((tag) => (
-                                    <PostTagItem
-                                        key={tag}
-                                        tag={tag}
-                                        isLabel={false}
-                                    />
-                                ))}
-                            </ul>
-                            <ul className="w-80">
-                                {thirdPart.map((tag) => (
-                                    <PostTagItem
-                                        key={tag}
-                                        tag={tag}
-                                        isLabel={false}
-                                    />
-                                ))}
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="category mt-6 flex md:flex-row">
+      <div className="flex w-10/12 max-w-1140px basis-2/3 rounded-xl bg-white px-12 pb-8 pt-8 shadow-0550 xs:min-w-[300px]">
+        <div className="flex flex-col">
+          <p className="flex pb-4 text-xl tracking-widest">{dict.category}</p>
+          <div className="flex flex-col">
+            <div className="flex xs:flex-col md:flex-row">
+              <ul className="w-80">
+                <PostTagItem tag="All" isLabel={false} />
+                {firstPart.map((tag) => (
+                  <PostTagItem key={tag} tag={tag} isLabel={false} />
+                ))}
+              </ul>
+              <ul className="w-80">
+                {secondPart.map((tag) => (
+                  <PostTagItem key={tag} tag={tag} isLabel={false} />
+                ))}
+              </ul>
+              <ul className="w-80">
+                {thirdPart.map((tag) => (
+                  <PostTagItem key={tag} tag={tag} isLabel={false} />
+                ))}
+              </ul>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
--- app/ui/posts/overview/post-item.tsx
+++ app/ui/posts/overview/post-item.tsx
@@ -12,7 +12,7 @@ export default function PostItem({ post }: { post: PostCard }) {
   const url_title = post.title.toLowerCase().replace(urlRegex, "-");
 
   return (
-    <li className="post-item mx-auto mb-6 flex min-h-[400px] w-full max-w-[400px] md:basis-1/2 lg:basis-1/3 list-none flex-col px-[11px] sm:w-1/3 md:w-1/2 md:max-w-none">
+  <li className="post-item mx-auto mb-6 flex min-h-[300px] w-full max-w-[400px] list-none flex-col px-[11px] sm:w-1/3 md:w-1/2 md:max-w-none md:basis-1/2 lg:basis-1/3">
       <article className="mx-auto block h-full w-full flex-row">
         <Link
           href={`${lang}/posts/${url_title}/${post.id}#blog-title`}
