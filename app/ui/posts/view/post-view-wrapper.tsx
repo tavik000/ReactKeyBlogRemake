31e922e7bc70ce197db557d1422a48d2df171e7f
Key <tavik002@gmail.com>
2024-10-02 15:20:38 +0900

    remove head

--- app/ui/posts/view/post-view-wrapper.tsx
+++ app/ui/posts/view/post-view-wrapper.tsx
@@ -1,14 +1,13 @@
 import { fetchPostById, fetchPostCommentByPostId } from "@/app/lib/data";
 import PostViewClient from "./post-view-client";
-import Head from "next/head";
 
 export async function generateMetadata({ postId, locale }: { postId: string; locale: string }) {
   const post = await fetchPostById(postId, locale);
 
   if (!post) {
     return {
-      title: 'Post not found',
-      description: 'The requested post could not be found.',
+      title: "Post not found",
+      description: "The requested post could not be found.",
     };
   }
 
@@ -39,24 +38,6 @@ export default async function PostViewWrapper({
   }
 
   const flattenedComments = comments.flat();
-  const { title, content } = post;
-  const limitedContent = content.split(" ").slice(0, 200).join(" ");
 
-  return (
-    <>
-      <Head>
-        <title>{title}</title>
-        <meta name="description" content={limitedContent} />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={limitedContent} />
-        <meta property="og:image" content="/opengraph-image.png" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@kiikey4" />
-        <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={limitedContent} />
-        <meta name="twitter:image" content="/opengraph-image.png" />
-      </Head>
-      <PostViewClient post={post} comments={flattenedComments} />
-    </>
-  );
+  return <PostViewClient post={post} comments={flattenedComments} />;
 }
