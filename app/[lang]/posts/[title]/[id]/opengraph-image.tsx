091ace501005abe733a18d7e30fa4f1b43e0542c
Key <tavik002@gmail.com>
2024-10-02 18:07:58 +0900

    smaller title padding

--- app/[lang]/posts/[title]/[id]/opengraph-image.tsx
+++ app/[lang]/posts/[title]/[id]/opengraph-image.tsx
@@ -64,7 +64,7 @@ export default async function Image({ params }: Props) {
             letterSpacing: "-0.025em", // tracking-tight
             color: "#ffbd15",
             textAlign: "left", // Align text to the left
-            marginBottom: "12px",
+            marginBottom: "6px",
           }}
         >
           Key Blog
