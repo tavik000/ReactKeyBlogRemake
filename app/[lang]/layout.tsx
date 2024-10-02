f84e0b4f9a1f3f2962e744f2192072dc419a662b
Key <tavik002@gmail.com>
2024-10-02 14:26:56 +0900

    layout metadata

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,31 +1,44 @@
-import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts';
-import { Metadata } from 'next';
-import Sky from '@/app/ui/sky';
-import PostSection from '@/app/ui/posts/general/post-section';
-import { getDictionary } from '@/app/components/localization/dictionaries';
-import { DictStructure } from '@/app/components/localization/dict-store';
-import { GetLocaleFromLang } from '@/app/lib/constants';
-import { auth } from '@/auth';
-import { NextUIProviderWrapper } from '@/app/components/NextUI/next-ui-providers-wrapper';
-import { LoginOpenFromPostProvider } from '@/app/components/context/login-open-from-post-provider';
-import { LocaleProvider } from '@/app/components/context/locale-provider';
-import { SessionProvider } from '@/app/components/context/session-provider';
-import LoginSuccessfulBanner from '@/app/ui/login-successful-banner';
-import { fetchAllNotificationByTargetUserName } from '../lib/data';
-import { NotificationProvider } from '@/app/components/context/notification-provider';
-import { Notification } from '@/app/lib/definitions';
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
+import { Metadata } from "next";
+import Sky from "@/app/ui/sky";
+import PostSection from "@/app/ui/posts/general/post-section";
+import { getDictionary } from "@/app/components/localization/dictionaries";
+import { DictStructure } from "@/app/components/localization/dict-store";
+import { GetLocaleFromLang } from "@/app/lib/constants";
+import { auth } from "@/auth";
+import { NextUIProviderWrapper } from "@/app/components/NextUI/next-ui-providers-wrapper";
+import { LoginOpenFromPostProvider } from "@/app/components/context/login-open-from-post-provider";
+import { LocaleProvider } from "@/app/components/context/locale-provider";
+import { SessionProvider } from "@/app/components/context/session-provider";
+import LoginSuccessfulBanner from "@/app/ui/login-successful-banner";
+import { fetchAllNotificationByTargetUserName } from "../lib/data";
+import { NotificationProvider } from "@/app/components/context/notification-provider";
+import { Notification } from "@/app/lib/definitions";
 
 export const experimental_ppr = true;
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://react-key-blog-remake.vercel.app/"),
   title: {
-    template: '%s | Acme Dashboard',
-    default: 'Acme Dashboard',
+    template: "%s | Key Blog",
+    default: "Key Blog",
   },
-  description: 'The official Next.js Learn Dashboard built with App Router.',
-  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  description: "Key Personal Blog for Game Development and Programming",
+  openGraph: {
+    title: 'Key Blog',
+    description: 'Key Personal Blog for Game Development and Programming',
+    images: '/opengraph-image.png'
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@eMartiiin94",
+    title: "Key Blog",
+    description: "Key Personal Blog for Game Development and Programming",
+    images: "/opengraph-image.png",
+  },
 };
+
 
 export default async function RootLayout({
   children,
@@ -40,15 +53,17 @@ export default async function RootLayout({
   const session = await auth();
   let notifications: Notification[] = [];
   if (session?.user) {
-    notifications = session?.user?.name ? (await fetchAllNotificationByTargetUserName(session.user.name) as unknown as Notification[]) : [];
+    notifications = session?.user?.name
+      ? ((await fetchAllNotificationByTargetUserName(
+          session.user.name,
+        )) as unknown as Notification[])
+      : [];
   }
-
-
 
   return (
     <html lang={lang}>
-      <body className={`${inter.className} antialiased w-full`}>
-        <div id="body-wrapper" className="overflow-hidden relative">
+      <body className={`${inter.className} w-full antialiased`}>
+        <div id="body-wrapper" className="relative overflow-hidden">
           <NextUIProviderWrapper>
             <LoginOpenFromPostProvider>
               <LocaleProvider inLocale={locale} inLang={lang} inDict={dict}>
@@ -56,9 +71,7 @@ export default async function RootLayout({
                   <NotificationProvider inNotifications={notifications || []}>
                     <LoginSuccessfulBanner />
                     <Sky />
-                    <PostSection>
-                      {children}
-                    </PostSection>
+                    <PostSection>{children}</PostSection>
                   </NotificationProvider>
                 </SessionProvider>
               </LocaleProvider>
