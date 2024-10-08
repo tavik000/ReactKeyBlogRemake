231442826ceabcfb3db8994ad4d452b37925aca5
Key <tavik002@gmail.com>
2024-10-08 22:16:30 +0900

    user email can be null and repeat

--- scripts/seed.js
+++ scripts/seed.js
@@ -20,7 +20,7 @@ async function seedUsers(client) {
       CREATE TABLE IF NOT EXISTS users (
         id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
-        email TEXT NOT NULL UNIQUE,
+        email TEXT,
         img VARCHAR(255) NOT NULL,
         theme VARCHAR(255) NOT NULL,
         last_login_date DATE NOT NULL,
