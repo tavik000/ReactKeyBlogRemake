9c600b514c2fc3b7e7b1d9185fa38d859ead0fbf
Key <tavik002@gmail.com>
2024-10-10 21:47:39 +0900

    add post not found at fetch id not found

--- app/ui/posts/view/post-view-wrapper.tsx
+++ app/ui/posts/view/post-view-wrapper.tsx
@@ -1,5 +1,8 @@
 import { fetchPostById, fetchPostCommentByPostId } from "@/app/lib/data";
 import PostViewClient from "./post-view-client";
+import { getDictionary } from "@/app/components/localization/dictionaries";
+import { DictStructure } from "@/app/components/localization/dict-store";
+import PostContentContainer from "./post-content-container";
 
 
 export default async function PostViewWrapper({
@@ -13,10 +16,24 @@ export default async function PostViewWrapper({
     fetchPostById(postId, locale),
     fetchPostCommentByPostId(postId),
   ]);
+  const dict = (await getDictionary(locale)) as DictStructure;
 
   if (!post) {
     console.error("Post not found, postId:", postId);
-    return <div>Post not found</div>;
+    return (
+    <div>
+      <PostContentContainer>
+        <div className="flex flex-col">
+          <p
+            id="post-title"
+            className="mt-2 flex justify-center text-lg font-semibold leading-normal"
+          >
+            {dict.post.postNotFound}
+          </p>
+        </div>
+      </PostContentContainer>
+    </div>
+    );
   }
 
   const flattenedComments = comments.flat();
