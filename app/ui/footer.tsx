64d3bdbc6bc64af9a587fa741babe5787cbfe0db
Key <tavik002@gmail.com>
2024-10-10 09:43:15 +0900

    fix footer button clickable area

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -5,19 +5,23 @@ import React from "react";
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <div id="section-footer" >
+    <div id="section-footer">
       <div className="footer-divider -translate-y-3" />
       <div className="flex flex-col py-4 text-center text-xs">
-        <Link href={homepageURL} className="mb-2 flex justify-center hover:underline">
-          Home
-        </Link>
-        <Link
-          href="//linktr.ee/keyzhao"
-          className="mb-2 flex justify-center hover:underline"
-        >
-          Contact
-        </Link>
-        <p className="flex justify-center mb-2">&copy; {year} KEY </p>
+        <div className="mb-2 flex justify-center">
+          <Link href={homepageURL} className="mb-2 flex justify-center hover:underline">
+            Home
+          </Link>
+        </div>
+        <div className="mb-2 flex justify-center">
+          <Link
+            href="//linktr.ee/keyzhao"
+            className="mb-2 flex justify-center hover:underline"
+          >
+            Contact
+          </Link>
+        </div>
+        <p className="mb-2 flex justify-center">&copy; {year} KEY </p>
       </div>
     </div>
   );
