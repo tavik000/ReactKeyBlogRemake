f0f7c227a07919ea4b0c23b2592f48ec7e5814fc
Key <tavik002@gmail.com>
2024-10-10 23:07:52 +0900

    tidy

--- app/ui/posts/overview/post-wrapper.tsx
+++ app/ui/posts/overview/post-wrapper.tsx
@@ -21,9 +21,9 @@ export default async function PostWrapper({
     return (
       <div className="category mt-6 flex md:flex-row">
         <div className="flex max-w-1140px rounded-xl bg-white px-12 pb-8 pt-8 shadow-0550 xs:w-11/12 xs:min-w-[300px] xl:w-10/12 xl:basis-2/3 dark:bg-zinc-900">
-          <div className="flex flex-col w-full">
-            <p className="flex text-lg items-center justify-center text-center">
-            {dict.overview.cannotFoundSearch} &quot;{query ? query : tag}&quot;
+          <div className="flex w-full flex-col">
+            <p className="flex items-center justify-center text-center text-lg">
+              {dict.overview.cannotFoundSearch} &quot;{query ? query : tag}&quot;
             </p>
           </div>
         </div>
