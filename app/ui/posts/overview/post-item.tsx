8e63e5fa577ee26fc688a884868ea4f895f88c5b
Key <tavik002@gmail.com>
2024-10-16 21:48:18 +0900

    tidy

--- app/ui/posts/overview/post-item.tsx
+++ app/ui/posts/overview/post-item.tsx
@@ -12,14 +12,14 @@ export default function PostItem({ post }: { post: PostCard }) {
   const url_title = post.title.toLowerCase().replace(urlRegex, "-");
 
   return (
-  <li className="post-item mx-auto mb-6 flex min-h-[300px] w-full max-w-[400px] list-none flex-col px-[11px] sm:w-1/3 md:w-1/2 md:max-w-none md:basis-1/2 lg:basis-1/3">
+    <li className="post-item mx-auto mb-6 flex min-h-[300px] w-full max-w-[400px] list-none flex-col px-[11px] sm:w-1/3 md:w-1/2 md:max-w-none md:basis-1/2 lg:basis-1/3">
       <article className="mx-auto block h-full w-full flex-row">
         <Link
           href={`${lang}/posts/${url_title}/${post.id}#blog-title`}
           className="article-link mx-auto flex h-full flex-row"
           prefetch={true}
         >
-          <div className="post-article flex h-full w-full flex-col justify-between rounded-xl dark:bg-zinc-900 bg-white px-3 pb-4 pt-6 font-bold shadow-0550 ">
+          <div className="post-article flex h-full w-full flex-col justify-between rounded-xl bg-white px-3 pb-4 pt-6 font-bold shadow-0550 dark:bg-zinc-900 ">
             <Image
               src={post.thumbnail_img}
               width={333}
@@ -37,12 +37,7 @@ export default function PostItem({ post }: { post: PostCard }) {
               </span>
               <span className="flex w-3/5 flex-wrap justify-end">
                 {post.tags.map((tag) => (
-                  <PostTagItem
-                    key={tag}
-                    tag={tag}
-                    isLabel={true}
-                    isClickable={false}
-                  />
+                  <PostTagItem key={tag} tag={tag} isLabel={true} isClickable={false} />
                 ))}
               </span>
             </span>
